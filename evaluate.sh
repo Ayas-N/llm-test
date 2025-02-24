a41dfb8e66cbc1f527f148b3f9fffdcd52073c19
Jey <chuahjeyson2003@gmail.com>
2025-02-24 16:17:42 +1100

    ok

--- evaluate.sh
+++ evaluate.sh
@@ -1,3 +1,14 @@
+for i in $(seq 1 5);
+do
+    mkdir -p sim{$i}
+    cd sim{$i}
+    mkdir -p agent_out
+    mkdir -p gpt_out
+    mkdir -p gpt_out
+    mkdir -p search_out
+    cd ..
+done
+
 python pipeline.py
 python search_only.py
 python pdf_only.py
